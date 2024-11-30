298b1c2cc42855a757987f1537860fece4f13296
ahmedali <ahmed1010ali>
2024-11-30 06:25:30 +0200

    new update

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import logging
+import os 
+from datetime import datetime
+
+
+# Generate log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+
+# Define log directory and ensure it exists
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Complete log file path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+
+# Configure logging 
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    ormat="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
